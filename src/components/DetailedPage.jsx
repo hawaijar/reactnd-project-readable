98bc15fae440dbee5fa86e07b4b51fe7019d3396
ksm <msushilk@in.ibm.com>
2017-09-11 10:24:19 +0530

    Added styling on user-comments

--- src/components/DetailedPage.jsx
+++ src/components/DetailedPage.jsx
@@ -1,36 +1,52 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import BackIcon from 'react-icons/lib/md/arrow-back';
-import avatarImage from './ninja_avatar.png';
-import { addComment } from '../actions';
-import '../App.css';
-import './DetailedPage.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import BackIcon from "react-icons/lib/md/arrow-back";
+import avatarImage from "./ninja_avatar.png";
+import avatar_user1 from "./avatar_user_1.jpg";
+import { addComment } from "../actions";
+import "../App.css";
+import "./DetailedPage.css";
 
 class DetailedPage extends Component {
 	state = {
 		comments: []
 	};
-	displayComments = (comments) => {
+	displayComments = comments => {
 		if (comments.length === 0) {
 			return null;
 		}
 
 		return comments.map((comment, index) => {
-			return <li key={index}>{comment}</li>
+			return (
+				<li className="comment user-comment" key={index}>
+					<div className="info">
+						<a href="#">Anie Silverston</a>
+						<span>4 hours ago</span>
+					</div>
+					<a className="avatar" href="#">
+						<img
+							src={avatar_user1}
+							width="35"
+							alt="Profile Avatar"
+							title="Anie Silverston"
+						/>
+					</a>
+					<p>{comment}</p>
+				</li>
+			);
 		});
-	
-		}
+	};
 
 	addComment = () => {
 		const postId = this.props.match.params.id;
 		this.props.pushComment(postId, this.textInput.value);
-		this.textInput.value = ''
-	}
+		this.textInput.value = "";
+	};
 
 	render() {
 		const { title, author, body } = this.props.post;
-		
+
 		const { comments } = this.props;
 		return (
 			<div>
@@ -41,19 +57,23 @@ class DetailedPage extends Component {
 				</div>
 				<div className="container">
 					<article className="post">
-						<h1>
-							{title}
-						</h1>
-						<p className="posted-by">
-							Posted by {author}
-						</p>
+						<h1>{title}</h1>
+						<p className="posted-by">Posted by {author}</p>
 						{body}
 					</article>
 					<hr />
 					<section className="commentContainer">
-						<span style={{ marginBottom: '.35em', fontSize: '1em', color: '#666' }}>Comments</span>
-						<section className="displayComment">
-							<ul>
+						<span
+							style={{
+								marginBottom: ".35em",
+								fontSize: "1em",
+								color: "#666"
+							}}
+						>
+							Comments
+						</span>
+						<section className="display-comment">
+							<ul className="comment-section">
 								{this.displayComments(comments)}
 							</ul>
 						</section>
