a0f6dba73165c3c56ad36cc247b6f5b63965ca78
Hawaijar.js <hawaijar.js@gmail.com>
2017-10-28 23:55:17 +0530

    Fixed/completed Post Form functionality

--- src/components/SemanticForm.jsx
+++ src/components/SemanticForm.jsx
@@ -1,106 +1,130 @@
-import React, { Component } from 'react';
-import { MenuItem, DropDownMenu, TextField } from 'material-ui';
+import React, {Component} from 'react';
+import {MenuItem, DropDownMenu, TextField} from 'material-ui';
 import uuid from 'uuid/v1';
-import { string, arrayOf } from 'prop-types';
+import {string, arrayOf} from 'prop-types';
 
 class SemanticForm extends Component {
-  state = {
-    selectedCategory: 1,
-    category: '',
-    title: '',
-    body: '',
-  };
-
-  onSubmit = (e) => {
-    e.preventDefault();
-    const post = {
-      id: uuid(),
-      title: this.title.value,
-      body: this.body.value,
-      timestamp: new Date().toString(),
-      voteScore: 1,
-      author: 'Stan Lee',
-      comments: [],
-      deleted: false,
-      category: this.category.value,
+    state = {
+        selectedCategory: 1,
+        category: '',
+        title: '',
+        body: '',
     };
-    this.props.onAdd(post);
-  };
-  getMenuItems = () => {
-    const { category, categories } = this.props;
-    let list = ['Select category'];
-    if (category.toLowerCase() === 'home') {
-      list = list.concat(categories);
-      const items = list.map((menuItem, index) =>
-        <MenuItem key={menuItem} value={index + 1} primaryText={menuItem} />);
-      return [...items];
+    constructor(props){
+        super();
+        this.mapCategories = props.categories.reduce((result, category, index)=> {
+            result[index+2] = category;
+            return result;
+        },{})
+        console.log(this.mapCategories)
     }
-    return (
-      <MenuItem
-        key={category}
-        value={1}
-        primaryText={`Category: ${category}`}
-      />
-    );
-  };
-    handleSelectChange = (event, index, value) =>
-      this.setState({ selectedCategory: value });
-    handleInputChange = (e, ref) => {
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        const post = {
+            id: uuid(),
+            title: this.state.title,
+            body: this.state.body,
+            timestamp: new Date().toString(),
+            voteScore: 1,
+            author: 'Stan Lee',
+            comments: [],
+            deleted: false,
+            category: this.mapCategories[this.state.selectedCategory],
+        };
+        this.props.onAdd(post);
     };
+    getMenuItems = () => {
+        const {category, categories} = this.props;
+        let list = ['Select category'];
+        if (category.toLowerCase() === 'home') {
+            list = list.concat(categories);
+            const items = list.map((menuItem, index) => {
+                if(index === 0) {
+                    return (<MenuItem disabled value={index + 1} primaryText={menuItem}/>)
+                }
+                else {
+                    return (<MenuItem key={menuItem} value={index + 1} primaryText={menuItem}/>)
+                }
+            });
+            return [...items];
+        }
+        return (
+            <MenuItem
+                key={category}
+                value={1}
+                primaryText={`Category: ${category}`}
+            />
+        );
+    };
+    handleSelectChange = (event, index, value) =>{
+        this.setState({selectedCategory: value});
+    };
+    handleInputChange = (e, ref) => {
+        const value = e.target.value;
+        this.setState({[ref]: value});
+    };
+
     render() {
-      const styles = {
-        container: {
-          marginTop: '-1em',
-        },
-        customWidth: {
-          width: 200,
-          marginLeft: '-24px',
-        },
-      };
-      return (
-        <div style={styles.container}>
-          <form onSubmit={this.onSubmit}>
-            <div>
-              <TextField
-                hintText="Enter title"
-                floatingLabelText="Title"
-                style={{ width: '80%' }}
-                onChange={e => this.handleInputChange(e, 'title')}
-              />
-              <br />
-              <TextField
-                hintText="Enter body"
-                floatingLabelText="Body"
-                multiLine
-                style={{ width: '80%' }}
-                onChange={e => this.handleInputChange(e, 'body')}
-              />
-              <br />
-              <DropDownMenu
-                disabled={this.props.category !== 'home'}
-                value={this.state.selectedCategory}
-                className="category-list"
-                style={styles.customWidth}
-                autoWidth={false}
-                onChange={this.handleSelectChange}
-              >
-                {this.getMenuItems()}
-              </DropDownMenu>
+        const styles = {
+            container: {
+                marginTop: '-1em',
+            },
+            customWidth: {
+                width: 200,
+                marginLeft: '-24px',
+            },
+        };
+        return (
+            <div style={styles.container}>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <TextField
+                            hintText="Enter title"
+                            floatingLabelText="Title"
+                            style={{width: '80%'}}
+                            onChange={e => this.handleInputChange(e, 'title')}
+                        />
+                        <br/>
+                        <TextField
+                            hintText="Enter body"
+                            floatingLabelText="Body"
+                            multiLine
+                            style={{width: '80%'}}
+                            onChange={e => this.handleInputChange(e, 'body')}
+                        />
+                        <br/>
+                        <DropDownMenu
+                            disabled={this.props.category !== 'home'}
+                            value={this.state.selectedCategory}
+                            className="category-list"
+                            style={styles.customWidth}
+                            autoWidth={false}
+                            onChange={this.handleSelectChange}
+                        >
+                            {this.getMenuItems()}
+                        </DropDownMenu>
+                        <div className="btn-group" role="group" aria-label="Basic example">
+                            <button onClick={this.onSubmit} type="button" className="btn btn-primary p-3">Submit</button>
+                            <button type="button" className="btn btn-secondary"
+                                    onClick={this.props.onModalClose}>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </form>
-        </div>
-      );
+        );
     }
 }
 
 SemanticForm.propTypes = {
-  category: string,
-  categories: arrayOf(string),
+    category: string,
+    categories: arrayOf(string),
 };
 SemanticForm.defaultProps = {
-  category: 'home',
-  categories: [],
+    category: 'home',
+    categories: [],
 };
 
 export default SemanticForm;
